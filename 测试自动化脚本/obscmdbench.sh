3ab1e2111d7c931d0b67f7f07fe04605f3dc6e2a
denghao-come <dh2913182454@163.com>
2023-01-06 16:32:01 +0800

    v2

--- 测试自动化脚本/obscmdbench.sh
+++ 测试自动化脚本/obscmdbench.sh
@@ -98,6 +98,7 @@ setup () {
 ===================================
 EOF
     read -p "please input a number：" x
+    echo "使用ansibe推送到所有节点主机: ansible -i hosts all -m copy -a 'src=/root/obscmdbench-master dest=/root/'  !!!!!!!!"
  
 case $x in
 	0)
