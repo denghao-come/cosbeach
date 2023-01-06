2867c77d9eac07a4b77368a6a40190be9cc2c10f
denghao-come <dh2913182454@163.com>
2023-01-06 17:39:39 +0800

    9项配置V5

--- 测试自动化脚本/obscmdbench.sh
+++ 测试自动化脚本/obscmdbench.sh
@@ -23,7 +23,7 @@ fi
 }
 
 function user(){
-    echo -e "\e[32m 输入对象存储用户名、ak、sk \e[0m"
+    echo -e "\e[36m 输入对象存储用户名、ak、sk \e[0m"
     read -p "input username: " username
     read -p "input ak: " ak
     read -p "input sk: "  sk
@@ -32,14 +32,14 @@ function user(){
 
 function OSCs() {
 ##修改链接IP
-echo -e "\e[32m 输入对象存储ipv6或者域名 \e[0m"
+echo -e "\e[36m 输入对象存储ipv6或者域名 \e[0m"
 read -p "input OSCs val:" OSCs
 sed -i '10c OSCs = '"${OSCs}"' ' /root/obscmdbench-master/config.dat
 }
 
 function testcase() {
 ##选择脚本运行模式
-echo -e "\e[32m 
+echo -e "\e[36m 
 201  上传文件
 202	 下载文件
 204	 删除文件 
@@ -50,42 +50,42 @@ sed -i '16c Testcase = '"${Testcase}"' ' /root/obscmdbench-master/config.dat
 
 function buckename(){
 ##修改存储桶
-echo -e "\e[32m 输入创建的桶 \e[0m"
+echo -e "\e[36m 输入创建的桶 \e[0m"
 read -p "input BucketName:" BucketName
 sed -i '402c BucketNameFixed = '"${BucketName}"' ' /root/obscmdbench-master/config.dat
 }
 
 function usedomainname(){
 ##修改是否启动域名
-echo -e "\e[32m 是否启动域名 \e[0m"
+echo -e "\e[36m 是否启动域名 \e[0m"
 read -p "input UseDomainName (请输入True 或者 false ):" UseDomainName
 sed -i '418c UseDomainName = '"${UseDomainName}"' ' /root/obscmdbench-master/config.dat
 }
 
 function domainname(){
 ##输入域名
-echo -e "\e[32m 输入域名名称 \e[0m"
+echo -e "\e[36m 输入域名名称 \e[0m"
 read -p "input DomainName :" UseDomainName
 sed -i '424c DomainName = '"${UseDomainName}"' ' /root/obscmdbench-master/config.dat
 }
 
 function objectsize(){
 ##文件大小字节
-echo -e "\e[32m 上传的对象大小（字节） \e[0m"
+echo -e "\e[36m 上传的对象大小（字节） \e[0m"
 read -p "input ObjectSize :" ObjectSize
 sed -i '192c ObjectSize =  '"${ObjectSize}"' ' /root/obscmdbench-master/config.dat
 }
 
 function objectsperbucketperthread(){
 ##对象数
-echo -e "\e[32m 每个并发在每个桶中上传的对象数 \e[0m"
+echo -e "\e[36m 每个并发在每个桶中上传的对象数 \e[0m"
 read -p "input ObjectsPerBucketPerThread :" ObjectsPerBucketPerThread
 sed -i '197c ObjectsPerBucketPerThread =  '"${ObjectsPerBucketPerThread}"' ' /root/obscmdbench-master/config.dat
 }
 
 function threadsperuser(){
 ##并发数
-echo -e "\e[32m 每个用户对应的的并发数 \e[0m"
+echo -e "\e[36m 每个用户对应的的并发数 \e[0m"
 read -p "input ThreadsPerUser :" ThreadsPerUser
 sed -i '29c ThreadsPerUser =  '"${ThreadsPerUser}"' ' /root/obscmdbench-master/config.dat
 }
@@ -162,6 +162,6 @@ esac
 }
  
 setup
- echo -e "\e[36m 使用ansibe推送到所有节点主机: ansible -i hosts all -m copy -a 'src=/root/obscmdbench-master dest=/root/'  !!!!!!!! \e[0m"
- echo -e "\e[36m 使用ansibe推送到所有节点主机: ps -ef |grep run.py|grep -v grep |awk '{print $2}'|xargs -I {} kill -9 {} \e[0m"
+ echo -e "\e[33m 使用ansibe推送到所有节点主机: ansible -i hosts all -m copy -a 'src=/root/obscmdbench-master dest=/root/' \e[0m"
+ echo -e "\e[33m 使用ansibe推送到所有节点主机: ps -ef |grep run.py|grep -v grep |awk '{print $2}'|xargs -I {} kill -9 {} \e[0m"
  
